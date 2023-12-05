5ef52a7975714fc3545e74b4054208553ff26f2a
John Cavanaugh <john.cavanaugh@hp.com>
2023-12-05 01:58:25 -0800

    merge

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,18 +1,18 @@
 return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- JC:  Hopefully we get kitty graphics protocol in windows Terminal
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+  },
   require("better_escape").setup {
     mapping = { "kj", "jj" }, -- a table with mappings to use
-    -- timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
+    timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
     clear_empty_lines = true, -- clear line after escaping if there is only whitespace
   },
-  --  require("nvim-ufo").setup { },
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
 }
