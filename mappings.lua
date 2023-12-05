9ddd0c72cad1161562f39a721e4e65f407f187b9
John Cavanaugh <john.cavanaugh@hp.com>
2023-12-05 15:46:06 -0800

    Explorer sidebar

--- mappings.lua
+++ mappings.lua
@@ -28,16 +28,37 @@ return {
     },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
-    ["<leader>b"] = { name = "Buffers" },
+    -- ["<leader>b"] = { name = "Buffers" },
+
     -- Quick Mappings
     -- quick save
     ["<C-s>"] = { ":w!<cr>", desc = "Save File" },
-    -- quick switch windows
+
+    -- quick switch windows (Im not so sure about this long term as it seems to conflict with other things)
     ["<tab><tab>"] = { ":wincmd p<cr>", desc = "Go to previous window" },
     ["<tab>l"] = { ":tabnext<cr>", desc = "Go to next tab" },
     ["<tab>h"] = { ":tabprev<cr>", desc = "Go to prev tab" },
+
     -- Support my old surround muscle memory
     ["<leader>s"] = { "gzaiw", desc = "Surround <nextchar>", remap = true },
+
+    -- Support Neotree Explorer favorites
+    ["<leader>e"] = {
+      "<cmd>Neotree toggle source=filesystem selector=false<cr> reveal_force_cwd",
+      desc = "Neotree Explorer (.)",
+    },
+    ["<leader>E"] = {
+      "<cmd>Neotree toggle source=filesystem selector=false<cr> ~",
+      desc = "Neotree Explorer (home)",
+    },
+    ["<leader>be"] = {
+      "<cmd>Neotree toggle source=buffers selector=false<cr> reveal",
+      desc = "Neotree Explorer (Buffers)",
+    },
+    ["<leader>ge"] = {
+      "<cmd>Neotree toggle source=git_status selector=false<cr> reveal",
+      desc = "Neotree Explorer (Git)",
+    },
     -- Faster access to common items
     ["<C-Space>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle Term" },
   },
