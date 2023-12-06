2fab6d8788f87c99ed5c23f341097cd8aeec029d
John Cavanaugh <john.cavanaugh@hp.com>
2023-12-06 12:56:44 -0800

    Neotree

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,17 +9,48 @@ return {
       -- JC:  Hopefully we get kitty graphics protocol in windows Terminal
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = false, -- when true, they will just be displayed differently than normal items
+          hide_dotfiles = true,
+          hide_gitignored = true,
+          hide_hidden = false, -- only works on Windows for hidden files/directories
+          hide_by_name = {
+            ".DS_Store",
+            "thumbs.db",
+            "node_modules",
+          },
+          hide_by_pattern = {
+            --"*.meta",
+            --"*/src/*/tsconfig.json",
+          },
+          always_show = { -- remains visible even if other settings would normally hide it
+            ".gitignore",
+            ".local",
+            ".config",
+          },
+          never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+            ".DS_Store",
+            "thumbs.db",
+          },
+          never_show_by_pattern = { -- uses glob style patterns
+            ".null-ls_*",
+          },
+        },
+      },
+    },
   },
   {
     "projekt0n/github-nvim-theme",
-    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
   },
 
   require("better_escape").setup {
-    mapping = { "kj", "jj" },   -- a table with mappings to use
+    mapping = { "kj", "jj" }, -- a table with mappings to use
     timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
-    clear_empty_lines = true,   -- clear line after escaping if there is only whitespace
+    clear_empty_lines = true, -- clear line after escaping if there is only whitespace
   },
 
   require("illuminate").configure {
