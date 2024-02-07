3b75b5241ceb92882e06e255937abe6c3f69746a
John Cavanaugh <john.cavanaugh@hp.com>
2024-02-06 22:21:16 -0800

    Change alpha

---
+++ plugins/alpha.lua
@@ -0,0 +1,26 @@
+return {
+  "goolord/alpha-nvim",
+  -- the first parameter is the plugin specification
+  -- the second is the table of options as set up in Lazy with the `opts` key
+  config = function(plugin, opts)
+    -- run the core AstroNvim configuration function with the options table
+    require "plugins.configs.alpha"(plugin, opts)
+
+    local dashboard = require "alpha.themes.dashboard"
+    dashboard.section.header.val = {
+      "                                                                       ",
+      "                                                                     ",
+      "       ████ ██████           █████      ██                     ",
+      "      ███████████             █████                             ",
+      "      █████████ ███████████████████ ███   ███████████   ",
+      "     █████████  ███    █████████████ █████ ██████████████   ",
+      "    █████████ ██████████ █████████ █████ █████ ████ █████   ",
+      "  ███████████ ███    ███ █████████ █████ █████ ████ █████  ",
+      " ██████  █████████████████████ ████ █████ █████ ████ ██████ ",
+      "                                                                       ",
+    }
+
+    dashboard.section.header.opts.hl = "DashboardHeader"
+    dashboard.section.footer.opts.hl = "DashboardFooter"
+  end,
+}
